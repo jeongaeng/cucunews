894eeb2219f521d55fce0014ede6c7371f34533d
전갱 <jeonkm1112@gmail.com>
2020-08-10 04:53:31 +0900

    수정본

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,14 +1,12 @@
 function initGnb() {
 	// 햄버거 메뉴 액션
 	var header = $('header#header');
-	var btnGnb = header.find('.btn-menu-open');
-	var globalUserNavi = $('nav#global-user-navi');
-	var gnb = globalUserNavi.find('.gnb');
-	// var g1Deps = gnb.find('>ul>li');
+	var btnOpen = header.find('.btn-menu-open');
+	var globalUserNavi = $('nav#gnb');
 	var btnClose = globalUserNavi.find('.btn-menu-close');
 	var isOpen = false;
 	
-	btnGnb.on('click',function() {
+	btnOpen.on('click',function() {
 		if(isOpen == false) {
 			$('html,body').css({
 				'overflow':'hidden'
@@ -76,8 +74,8 @@ $(document).ready(function() {
 		autoplaySpeed: 3000,
 	});
 
-	// 이슈리스트 슬라이드
-	$('.article-list-container').slick({
+	// 뉴스리스트 슬라이드
+	$('.news-slide-wrap').slick({
 		dots: true,
 	});
 })
